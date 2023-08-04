9a189d04187994de77a1a72bc4fa809fdfad5db7
Sergei Blockiy <sergei.blockiy@gmail.com>
2023-08-04 10:57:03 +0300

    add sound source code

---
+++ Assets/Scripts/AudioPlay.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class AudioPlay : MonoBehaviour
+    {
+        [SerializeField] private AudioClip hurtSound;
+        [SerializeField] private AudioSource soundSource;
+        private void Start()
+        {
+            soundSource.Play();
+        }
+    }
+}
